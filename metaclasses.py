739007e9183adcb4f7d94a287a0ea7bf3ec20ca9
revillnik <revillnik@gmail.com>
2024-01-07 20:41:05 +0300

    next

--- metaclasses.py
+++ metaclasses.py
@@ -1,5 +1,22 @@
 def sum(self):
-	return self.max + self.min
-P = type("Point", (), {'max': 100, 'min': 5, 'sum': sum})
+    return self.max + self.min
+
+
+P = type("Point", (), {"max": 100, "min": 5, "sum": sum})
 a = P()
 print(a.sum())
+
+
+class Meta(type):
+    def __init__(cls, name, base, attrs):
+        cls.max = 10
+        cls.min = 0
+
+
+class Point(metaclass=Meta):
+    def get_coords(self):
+        return (0, 0)
+
+
+b = Point()
+print(b.min)
